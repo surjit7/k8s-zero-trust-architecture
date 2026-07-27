9c0ab764b8fed3c31dfaceb5b759d650b08376fb
surjitsingh_bjs <surjit.singh@bjshomedelivery.com>
2026-07-27 19:09:59 +0530

    app using redis headless service

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from werkzeug.middleware.proxy_fix import ProxyFix
+import redis
 
 app = Flask(__name__)
 
@@ -7,11 +8,14 @@ app = Flask(__name__)
 # x_for=1 tells it to trust the first IP in the X-Forwarded-For header
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
+# Connect DIRECTLY to the Redis Pod using its Headless DNS name
+cache = redis.Redis(host='redis-0.redis-headless.default.svc.cluster.local', port=6379)
+
 @app.route('/')
 def hello():
     # Because of ProxyFix, remote_addr now contains the real client IP!
     client_ip = request.remote_addr
-    return f"Hello from Dockerized Flask! Your real IP is: {client_ip}"
-
+    hits = cache.incr('hits')
+    return f"Hello from GitOps Flask! Your real IP is: {client_ip}. You are visitor #{hits}."
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
